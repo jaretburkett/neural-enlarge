6881867cc180dfece0dc872fc21c9e4e43714e7b
Jaret Burkett <jaretburkett@gmail.com>
2018-05-14 07:56:23 -0500

    Added more filters to magic

--- magic.py
+++ magic.py
@@ -1,8 +1,8 @@
-import os
 import cv2
 import random
 import numpy as np
 import PIL
+from PIL import ImageEnhance
 
 
 def pil_to_cv(img):
@@ -19,31 +19,31 @@ def cv_to_pil(img):
 def add_random_motion_blur(img):
 
     cv_img = pil_to_cv(img)
-    size = random.randint(1, 30)
-    type = random.randint(0, 3)
+    size = random.randint(1, 10)
+    blur_direction = random.randint(0, 3)
 
     kernel_motion_blur = np.zeros((size, size))
 
     # horizontal
-    if type == 0:
+    if blur_direction == 0:
         kernel_motion_blur[int((size - 1) / 2), :] = np.ones(size)
 
-    # verticle
-    if type == 1:
+    # vertical
+    if blur_direction == 1:
         i = 0
         while i < len(kernel_motion_blur):
             kernel_motion_blur[i][int((size - 1) / 2)] = np.ones(1)
             i += 1
 
     # top left to bottom right
-    if type == 2:
+    if blur_direction == 2:
         i = 0
         while i < len(kernel_motion_blur):
             kernel_motion_blur[i][i] = np.ones(1)
             i += 1
 
     # top right to bottom left
-    if type == 3:
+    if blur_direction == 3:
         i = 0
         while i < len(kernel_motion_blur):
             kernel_motion_blur[i][size - i - 1] = np.ones(1)
@@ -56,6 +56,55 @@ def add_random_motion_blur(img):
     return cv_to_pil(cv_img)
 
 
+def adjust_random_color(img):
+    adjust = random.randrange(10, 20)
+    adjust = adjust / 15
+    contrast_image = ImageEnhance.Color(img)
+    return contrast_image.enhance(adjust)
+
+
+def adjust_random_contrast(img):
+    adjust = random.randrange(10, 20)
+    adjust = adjust / 15
+    contrast_image = ImageEnhance.Contrast(img)
+    return contrast_image.enhance(adjust)
+
+
+def adjust_random_brightness(img):
+    adjust = random.randrange(10, 20)
+    adjust = adjust / 15
+    contrast_image = ImageEnhance.Brightness(img)
+    return contrast_image.enhance(adjust)
+
+
+def adjust_random_sharpness(img):
+    adjust = random.randrange(0, 15)
+    contrast_image = ImageEnhance.Sharpness(img)
+    return contrast_image.enhance(adjust)
+
+
 def random_magic(img):
-    return add_random_motion_blur(img)
+    one_in = 3
+
+    # color
+    if random.randint(1, one_in) == 1:
+        img = adjust_random_color(img)
+
+    # contrast
+    if random.randint(1, one_in) == 1:
+        img = adjust_random_contrast(img)
+
+    # brightness
+    if random.randint(1, one_in) == 1:
+        img = adjust_random_brightness(img)
+
+    # sharpness
+    if random.randint(1, one_in) == 1:
+        img = adjust_random_sharpness(img)
+
+    # motion blur
+    if random.randint(1, one_in) == 1:
+        img = add_random_motion_blur(img)
+
+    return img
 
